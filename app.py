a28225ca345a12b314aeed732f744daddfb3de4f
Rahul Raj <rahul.raj@jktech.com>
2023-08-23 18:50:13 +0530

    updated

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 import datetime
 from collections import OrderedDict
-
 import pandas as pd
-import shap
 import streamlit as st
-from matplotlib import pyplot as plt
-
-
-
 st.set_page_config(layout="wide")
 """
 if not model.is_model_trained():
@@ -61,21 +55,11 @@ loan_request = get_loan_request()
 df = pd.DataFrame.from_dict(loan_request)
 print(loan_request)
 
-# Full feature vector
-st.header("Feature vector (user input + zipcode features + user features):")
-vector = model._get_online_features_from_feast(loan_request)
-ordered_vector = loan_request.copy()
-key_list = vector.keys()
-key_list = sorted(key_list)
-for vector_key in key_list:
-    if vector_key not in ordered_vector:
-        ordered_vector[vector_key] = vector[vector_key]
-df = pd.DataFrame.from_dict(ordered_vector)
 
 
 # Results of prediction
 st.header("Application Status (model prediction):")
-result = model.predict(loan_request)
+result = 0
 
 if result == 0:
     st.success("Your loan has been approved!")
@@ -83,20 +67,3 @@ elif result == 1:
     st.error("Your loan has been rejected!")
 
 
-# Feature importance
-st.header("Feature Importance")
-X = pd.read_parquet("data/training_dataset_sample.parquet")
-explainer = shap.TreeExplainer(model.classifier)
-shap_values = explainer.shap_values(X)
-left, mid, right = st.columns(3)
-with left:
-    plt.title("Feature importance based on SHAP values")
-    shap.summary_plot(shap_values[1], X)
-    st.set_option("deprecation.showPyplotGlobalUse", False)
-    st.pyplot(bbox_inches="tight")
-    st.write("---")
-
-with mid:
-    plt.title("Feature importance based on SHAP values (Bar)")
-    shap.summary_plot(shap_values, X, plot_type="bar")
-    st.pyplot(bbox_inches="tight")
